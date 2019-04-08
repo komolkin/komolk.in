d043a6fcaf06f318dc4c99e7492ca8903456cf07
Ilya Komolkin <komolkin@gmail.com>
2019-04-08 23:20:52 +0300

    new js file

---
+++ js/fade.js
@@ -0,0 +1,17 @@
+<script>
+    var classes = [
+        '☻',
+        'Hi',
+        'Im Ilya, Product Designer',  
+        'addicted to simplicity',
+        'and aesthetic'
+    ];
+
+    (function displayClass(i) {
+        $('#tst h3').text(classes[i]).fadeIn(1000, function() {
+            $(this).delay(600).fadeOut(1000, function() {
+                displayClass((i + 1) % classes.length);
+            });
+        });
+    })(0);
+</script>
