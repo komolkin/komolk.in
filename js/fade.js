c79256f81fb9cd48379063efaa2a82a9ecab8955
Ilya Komolkin <komolkin@gmail.com>
2019-04-08 23:29:59 +0300

    fix

--- js/fade.js
+++ js/fade.js
@@ -1,14 +1,15 @@
 var classes = [
     '☻',
     'Hi',
-    'Im Ilya, Product Designer',  
+    'Im Ilya',
+    'Product Designer',
     'addicted to simplicity',
     'and aesthetic'
 ];
 
 (function displayClass(i) {
-    $('#tst h3').text(classes[i]).fadeIn(1000, function() {
-        $(this).delay(600).fadeOut(1000, function() {
+    $('#tst h3').text(classes[i]).fadeIn(500, function() {
+        $(this).delay(6000).fadeOut(500, function() {
             displayClass((i + 1) % classes.length);
         });
     });
