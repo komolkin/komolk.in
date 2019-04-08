9eaf032f0ef99a15cfe4a6e2a7103ab723a71fd4
Ilya Komolkin <komolkin@gmail.com>
2019-04-09 01:49:36 +0300

    fixed delay

--- js/fade.js
+++ js/fade.js
@@ -5,7 +5,7 @@ var classes = [
 
 (function displayClass(i) {
     $('#tst h3').text(classes[i]).fadeIn(500, function() {
-        $(this).delay(6000).fadeOut(500, function() {
+        $(this).delay(10000).fadeOut(500, function() {
             displayClass((i + 1) % classes.length);
         });
     });
