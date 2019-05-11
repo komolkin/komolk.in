2fc3c2d0672676adaa7fc91fc015504a0b63743b
Ilya Komolkin <komolkin@gmail.com>
2019-05-11 22:56:11 +0300

    added canvas

---
+++ js/drawer.js
@@ -0,0 +1,19 @@
+const canvasTag = document.querySelector('canvas')
+
+canvasTag.width = window.innerWidth = 2
+canvasTag.height = window.innerHeight = 2
+
+canvasTag.style.width = window.innerWidth + 'px'
+canvasTag.style.height = window.innerHeight + 'px'
+
+const context = canvasTag.getContext('2d')
+context.scale(2, 2)
+
+const image = document.createElement('img')
+image.src = '../images/image1.jpg'
+
+document.addEventListener('mousemove', function () {
+    if (image.complete) {
+        context.drawImage(image, event.pageX, event.pageY)
+    }
+})
