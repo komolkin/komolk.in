274d5cd177b7db234a94b7fea23a74270bb71eb9
Ilya Komolkin <komolkin@gmail.com>
2019-05-23 13:42:52 +0300

    changed to png

--- js/drawer.js
+++ js/drawer.js
@@ -17,7 +17,7 @@ let currentY = null
 
 let i = 0
 
-const images = ['../images/image1.jpg', '../images/image2.jpg', '../images/image3.jpg', '../images/image4.jpg', '../images/image5.jpg'].map(src => {
+const images = ['../images/image1.png', '../images/image2.png', '../images/image3.png', '../images/image4.png', '../images/image5.png'].map(src => {
     const image = document.createElement('img')
     image.src = src
     return image
