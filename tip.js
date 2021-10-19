68e009997dbd840e5b13a16ca3fa274af8ff4423
Ilya Komolkin <komolkin@gmail.com>
2021-10-19 21:36:33 +0300

    fix

--- tip.js
+++ tip.js
@@ -1,9 +1,25 @@
+const web3 = new Web3(Web3.givenProvider)
+
 const form = document.querySelector("form")
 
 const send = async function (amount) {
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
-    alert(accounts)
+
+    // alert(accounts)
     // alert("You are going to send " + amount + " ETH")
+
+    const wei = web3.utils.toWei(amount, "ether")
+
+    if (accounts.length > 0) {
+        window.ethereum.request({
+            method: "eth_sendTransaction",
+            params: [{
+                from: accounts[0],
+                to: "0x383351E7787347fC7CfE44561c645D6b382e4525",
+                value: wei
+            }]
+        })
+    }
 }
 
 form.addEventListener("submit", function (event) {
