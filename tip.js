a0f4490b6d0f545e85b51792f7668ffdb7597f7b
Ilya Komolkin <komolkin@gmail.com>
2021-10-19 21:00:13 +0300

    fix

--- tip.js
+++ tip.js
@@ -1,9 +1,13 @@
 const form = document.querySelector("form")
 
+const send = function () {
+    form.style.background = "red"
+}
+
 form.addEventListener("submit", function (event) {
     event.preventDefault()
     if (window.ethereum) {
-        form.style.background = "red"
+        send()
     } else {
         alert("Please install an Ethereum wallet")
     }
