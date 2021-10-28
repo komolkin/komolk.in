f221e3506e13c6bd649ba734cecaf99ff111d457
Ilya Komolkin <komolkin@gmail.com>
2021-10-29 02:59:52 +0300

    fix

---
+++ coin.js
@@ -0,0 +1,17 @@
+const url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+const priceTag = document.querySelector("h1")
+let currency = "USD"
+
+const checkPrice = function () {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            priceTag.innerHTML = data.bpi.USD.rate_float.toFixed(1)
+        })
+}
+
+checkPrice()
+
+setInterval(function () {
+    checkPrice()
+}, 60000)
