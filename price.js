dede612ac60e4a048088436dea8cd03552a13388
Ilya Komolkin <komolkin@gmail.com>
2021-11-14 02:32:10 +0300

    fix

--- price.js
+++ price.js
@@ -2,6 +2,8 @@ const ws = new WebSocket('wss://ws.coincap.io/prices?assets=ethereum')
 let stockPriceElement = document.getElementById('stock-price')
 
 ws.onmessage = function (msg) {
-  let stockObject = JSON.parse(msg.data);
-  stockPriceElement.innerText = stockObject.ethereum;
+  let stockObject = JSON.parse(msg.data)
+  stockPriceElement.innerText = stockObject.ethereum
 }
+
+// .substring(0, 4)
