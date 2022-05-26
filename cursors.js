7c3800bae26c78930839a8c66e74cd2110329eee
Ilya Komolkin <komolkin@gmail.com>
2022-05-26 15:43:21 +0300

    fix

--- cursors.js
+++ cursors.js
@@ -1,25 +1,25 @@
-const cursorTag = document.querySelector("div.cursors")
-const ball = cursorTag.querySelector("div")
+const cursorTag = document.querySelector("div.cursors");
+const ball = cursorTag.querySelector("div");
 
-let currentX = 0
-let currentY = 0
-let aimX = 0
-let aimY = 0
-let speed = 0.2
+let currentX = 0;
+let currentY = 0;
+let aimX = 0;
+let aimY = 0;
+let speed = 0.2;
 
 const animate = function () {
-    currentX += (aimX - currentX) * speed
-    currentY += (aimY - currentY) * speed
+  currentX += (aimX - currentX) * speed;
+  currentY += (aimY - currentY) * speed;
 
-    ball.style.left = currentX + "px"
-    ball.style.top = currentY + "px"
+  ball.style.left = currentX + "px";
+  ball.style.top = currentY + "px";
 
-    requestAnimationFrame(animate)
-}
+  requestAnimationFrame(animate);
+};
 
-animate()
+animate();
 
 document.addEventListener("mousemove", function (event) {
-    aimX = event.pageX
-    aimY = event.pageY
-})
+  aimX = event.pageX;
+  aimY = event.pageY;
+});
