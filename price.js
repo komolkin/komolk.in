38ed684e585a587f5219edc07a25a93bb98ed98b
Ilya Komolkin <komolkin@gmail.com>
2022-07-25 00:42:54 +0300

    Added Heart Rate

--- price.js
+++ price.js
@@ -1,13 +1,13 @@
-const ws = new WebSocket('wss://ws.coincap.io/prices?assets=ethereum')
-let stockPriceDollars = document.getElementById('stock-price')
-let stockPriceCents = document.getElementById('stock-price-cents')
+const ws = new WebSocket("wss://ws.coincap.io/prices?assets=ethereum");
+let stockPriceDollars = document.getElementById("stock-price");
+let stockPriceCents = document.getElementById("stock-price-cents");
 
 ws.onmessage = function (msg) {
-  let stockDollars = JSON.parse(msg.data)
-  stockPriceDollars.innerText = stockDollars.ethereum.substring(0, 4)
+  let stockDollars = JSON.parse(msg.data);
+  stockPriceDollars.innerText = stockDollars.ethereum.substring(0, 4);
 
-  let stockCents = JSON.parse(msg.data)
-  stockPriceCents.innerText = stockCents.ethereum.substring(4, 7)
-}
+  let stockCents = JSON.parse(msg.data);
+  stockPriceCents.innerText = stockCents.ethereum.substring(4, 7);
+};
 
 // .substring(0, 4)
